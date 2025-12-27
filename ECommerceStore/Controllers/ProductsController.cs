764cbb2cb92431e69c7cac60decfc10709ca7850
Seif Essam <seifessam60@gmail.com>
2025-12-27 22:33:22 +0200

    Add Create & Edit product functionality

--- ECommerceStore/Controllers/ProductsController.cs
+++ ECommerceStore/Controllers/ProductsController.cs
@@ -1,5 +1,7 @@
 ﻿using ECommerceStore.Data;
+using ECommerceStore.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 
@@ -32,6 +34,78 @@ namespace ECommerceStore.Controllers
                 return NotFound();
             }
             return View(product);
+        }
+        public IActionResult Create()
+        {
+            ViewBag.Categories = new SelectList(_context.Categories, "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                product.CreatedAt = DateTime.Now;
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Categories = new SelectList(_context.Categories, "Id", "Name", product.CategoryId);
+            return View(product);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var product = await _context.Products.FindAsync(id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            ViewBag.Categories = new SelectList(_context.Categories, "Id", "Name", product.CategoryId);
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Product product)
+        {
+            if(id != product.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Products.Update(product);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+
+                    if (!ProductExists(product.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.Categories = new SelectList(_context.Categories, "Id", "Name", product.CategoryId);
+            return View(product);
+        }
+        public bool ProductExists(int id)
+        {
+            return _context.Products.Any(p => p.Id == id);
         }
     }
 }
--- ECommerceStore/Models/Product.cs
+++ ECommerceStore/Models/Product.cs
@@ -1,15 +1,24 @@
-﻿namespace ECommerceStore.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerceStore.Models
 {
     public class Product
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        [Required(ErrorMessage = "Name is Required" )]
+        public string Name { get; set; } = string.Empty;
+        [Required(ErrorMessage ="Description is Required")]
+        public string Description { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Price is Required")]
+        [Range(0.01, double.MaxValue, ErrorMessage ="Price Should Be Larger Than 0")]
         public decimal Price { get; set; }
-        public string ImageUrl { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Stock is Required")]
+        [Range(0,int.MaxValue, ErrorMessage = "Stock Must be 0 or Bigger")]
         public int Stock { get; set; }
+        [Required(ErrorMessage ="Category is Required")]
         public int CategoryId { get; set; }
-        public Category Category { get; set; }
+        public Category? Category { get; set; }
         public DateTime CreatedAt { get; set; }
     }
 }
